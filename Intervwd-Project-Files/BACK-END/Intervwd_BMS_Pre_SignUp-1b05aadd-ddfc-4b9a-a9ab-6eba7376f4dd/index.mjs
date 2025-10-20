2f833431261b477cba2a398ea213f72c9b9b0a94
YourGitHubUsername <mgmanoj1481@gmail.com>
2025-10-20 19:36:20 +0530

    Initial commit1

--- Intervwd-Project-Files/BACK-END/Intervwd_BMS_Pre_SignUp-1b05aadd-ddfc-4b9a-a9ab-6eba7376f4dd/index.mjs
+++
@@ -1,6 +0,0 @@
-export const handler = async (event) => {
-  console.log(JSON.stringify(event));
-  event.response.autoConfirmUser = true;
-  event.response.autoVerifyEmail = true;
-  return event;
-};
